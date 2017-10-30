42c3719f8d81ea86f50c412ddf308b7223374500
Rache Bartmoss <bartmoss@tutanota.com>
2017-10-30 13:45:13 +0100

    Trying to initialize Postgres docker instance with scripts

--- test/createdb.sh
+++ test/createdb.sh
@@ -1,11 +1,11 @@
-sudo -u postgres dropdb testdb
-sudo -u postgres createdb testdb
-sudo -u postgres psql testdb -c "DROP USER IF EXISTS testuser"
-sudo -u postgres psql testdb -c "CREATE USER testuser PASSWORD 'testpass'"
-# sudo -u postgres createuser testuser
-sudo -u postgres psql testdb -c "GRANT ALL PRIVILEGES ON DATABASE testdb TO testuser"
+dropdb testdb
+createdb testdb
+psql testdb -c "DROP USER IF EXISTS testuser"
+psql testdb -c "CREATE USER testuser PASSWORD 'testpass'"
+# su -u postgres createuser testuser
+psql testdb -c "GRANT ALL PRIVILEGES ON DATABASE testdb TO testuser"
 
-sudo -u postgres psql testdb -f test/testdb.sql
+psql testdb -f test/testdb.sql
 
-sudo -u postgres psql testdb -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO testuser"
-sudo -u postgres psql testdb -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO testuser"
+psql testdb -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO testuser"
+psql testdb -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO testuser"
