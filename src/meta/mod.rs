3034a62f5342cb3f46f03f4e3f8a4f8d762c8232
Rache Bartmoss <bartmoss@tutanota.com>
2017-11-08 07:51:38 +0100

    Initial version of metainfo traits over serializable metainfo structs work, array/object still needs consideration

--- src/meta/mod.rs
+++ src/meta/mod.rs
@@ -12,7 +12,7 @@ pub trait Data
 {
     fn hash(&self) -> &[u8]; // of blob data
     fn blob(&self) -> &[u8];
-    fn attributes<'a>(&'a self) -> Box< Iterator< Item = &'a Attribute> + 'a >;
+    fn attributes<'a>(&'a self) -> Box< Iterator<Item = &'a Attribute> + 'a >;
     // TODO add multicodec query here
     // fn format(&self) -> FormatId;
 }
@@ -20,11 +20,25 @@ pub trait Data
 pub trait Attribute
 {
     fn name(&self)  -> &str;
-    fn value(&self) -> &AttributeValue;
+    fn value<'a>(&'a self) -> AttributeValue<'a>;
 }
 
 
-#[derive(Serialize, Deserialize)]
+pub enum AttributeValue<'a>
+{
+    Boolean(bool),
+    Integer(i64),
+    Float(f64),
+    String(&'a str),
+    Location(&'a GpsLocation),
+    Timestamp(&'a SystemTime),
+    Link(&'a Link),
+//    Array(&'a Iterator<Item = AttributeValue<'a>>),
+//    Object(&'a Iterator<Item = &'a Attribute>),
+}
+
+
+#[derive(Debug, Copy, Clone, Serialize, Deserialize, PartialEq)]
 pub enum StorageId
 {
     // TODO consider possible values
@@ -43,19 +57,6 @@ pub struct GpsLocation
     longitude:  f64,
 }
 
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
-pub enum AttributeValue//<'a>
-{
-    BOOLEAN(bool),
-    INT(i64),
-    FLOAT(f64),
-    STRING(String),
-    LOCATION(GpsLocation),
-    TIMESTAMP(SystemTime),
-//    LINK( &'a Link ),
-//    ARRAY( &'a [&'a Attribute] ),
-//    OBJECT( &'a Iterator<Item=&'a Attribute> ),
-}
 
 
 
@@ -67,29 +68,84 @@ mod tests
     //use async::imp::*;
 
 
+    #[derive(Debug, PartialEq, Serialize, Deserialize)]
+    struct MetaLink
+    {
+        hash:    Vec<u8>,
+        storage: StorageId,
+    }
 
-    #[derive(Debug, Serialize, Deserialize)]
+    impl MetaLink
+    {
+        fn new(hash: Vec<u8>, storage: StorageId) -> Self
+            { Self{hash: hash, storage: storage} }
+    }
+
+    impl Link for MetaLink
+    {
+        fn hash(&self)    -> &[u8]      { self.hash.as_ref() }
+        fn storage(&self) -> StorageId  { self.storage }
+    }
+
+
+    #[derive(Debug, Serialize, Deserialize, PartialEq)]
+    enum MetaAttrVal
+    {
+        BOOL(bool),
+        INT(i64),
+        FLOAT(f64),
+        STRING(String),
+        LOCATION(GpsLocation),
+        TIMESTAMP(SystemTime),
+        LINK(MetaLink),
+//        ARRAY( Vec<MetaAttrVal> ),
+//        OBJECT( Vec<MetaAttr> ),
+    }
+
+    impl MetaAttrVal
+    {
+        fn to_attr_val<'a>(&'a self) -> AttributeValue<'a>
+        {
+            match *self {
+                MetaAttrVal::BOOL(v)            => AttributeValue::Boolean(v),
+                MetaAttrVal::INT(v)             => AttributeValue::Integer(v),
+                MetaAttrVal::FLOAT(v)           => AttributeValue::Float(v),
+                MetaAttrVal::STRING(ref v)      => AttributeValue::String(&v),
+                MetaAttrVal::LOCATION(ref v)    => AttributeValue::Location(&v),
+                MetaAttrVal::TIMESTAMP(ref v)   => AttributeValue::Timestamp(&v),
+                MetaAttrVal::LINK(ref v)        => AttributeValue::Link(v),
+//                MetaAttrVal::ARRAY(ref v)       => AttributeValue::Array( &v.iter().map( |m| m.to_attr_val() ) ),
+//                MetaAttrVal::OBJECT(ref v)      => AttributeValue::Object( &v.iter().map( |m| m as &Attribute) ),
+            }
+        }
+    }
+
+
+
+    #[derive(Debug, Serialize, Deserialize, PartialEq)]
     struct MetaAttr
     {
         name:   String,
-        value:  AttributeValue,
+        value:  MetaAttrVal,
     }
 
     impl MetaAttr
     {
-        pub fn new(name: &str, value: AttributeValue) -> Self
+        pub fn new(name: &str, value: MetaAttrVal) -> Self
             { Self{ name: name.to_owned(), value: value } }
     }
 
     impl Attribute for MetaAttr
     {
-        fn name(&self)  -> &str             { &self.name }
-        fn value(&self) -> &AttributeValue  { &self.value }
+        fn name(&self) -> &str { &self.name }
+
+        fn value<'a>(&'a self) -> AttributeValue<'a>
+            { self.value.to_attr_val() }
     }
 
 
 
-    //#[derive(Debug, Serialize, Deserialize)]
+    #[derive(Debug, Serialize, Deserialize)]
     struct MetaData
     {
         blob:   Vec<u8>,
@@ -121,19 +177,25 @@ mod tests
     #[test]
     fn test_metadata()
     {
+        let linkhash = b"Far far away in another storage network".to_vec();
         let attrs = vec!(
-            MetaAttr::new( "test", AttributeValue::BOOLEAN(true) ),
-            MetaAttr::new( "timestamp", AttributeValue::TIMESTAMP( SystemTime::now() ) ),
+            MetaAttr::new( "works", MetaAttrVal::BOOL(true) ),
+            MetaAttr::new( "timestamp", MetaAttrVal::TIMESTAMP( SystemTime::now() ) ),
+            MetaAttr::new( "link", MetaAttrVal::LINK( MetaLink::new(linkhash, StorageId::InMemory) ) ),
         );
         let blob = b"1234567890abcdef".to_vec();
         let hash = b"qwerty".to_vec();
         let metadata = MetaData::new(blob, hash, attrs);
 
         let test_attr : Vec<&Attribute> = metadata.attributes()
-            .filter( |attr| attr.name() == "test" )
+            .filter( |attr| attr.name() == "works" )
             .collect();
         assert_eq!( test_attr.len(), 1 );
-        assert_eq!( test_attr.get(0).unwrap().name(), "test" );
-        assert_eq!( *test_attr.get(0).unwrap().value(), AttributeValue::BOOLEAN(true) );
+        assert_eq!( test_attr.get(0).unwrap().name(), "works" );
+
+        match test_attr.get(0).unwrap().value() {
+            AttributeValue::Boolean(v) => assert!(v),
+            _ => assert!(false),
+        }
     }
 }
