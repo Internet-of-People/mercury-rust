01f45c5da85cd903d54fc706ff99a80b1e0c43ee
Rache Bartmoss <bartmoss@tutanota.com>
2018-03-01 10:16:56 +0100

    Working on interfaces

--- src/lib.rs
+++ src/lib.rs
@@ -10,79 +10,162 @@ use tokio_core::net::TcpStream;
 
 
 
-type ProfileId = Vec<u8>;
-
-enum ProfileLookupError
+pub enum SearchProfileError
 {
     TODO // TODO
 }
 
-fn find_profile( /* TODO what filter criteria should we have here? */ ) ->
-Box< Future<Item=ProfileId, Error=ProfileLookupError> >
-{
-    Box::new( future::err(ProfileLookupError::TODO) )
-}
-
-
-
-enum ContactLookupError
+pub enum SearchAddressError
 {
     TODO // TODO
 }
 
-// TODO should we use Stream<Multiaddr> here instead of Vec<>?
-fn find_contacts(profile_id: ProfileId) ->
-Box< Future<Item=Vec<Multiaddr>, Error=ContactLookupError> >
-{
-    Box::new( future::err(ContactLookupError::TODO) )
-}
-
-
-
-enum AddressConnectError
+pub enum OpenAddressError
 {
     TODO // TODO
 }
 
-// TODO for Tor, I2P and similar cases, the TcpStream return type might not work out,
-// maybe we'll need something like (Stream<u8>,Sink<u8>) or something similar
-fn connect_address(multiaddr: Multiaddr) ->
-Box< Future<Item=TcpStream, Error=AddressConnectError> >
+pub enum ConnectToProfileError
 {
-    Box::new( future::err(AddressConnectError::TODO) )
-}
-
-
-
-enum ProfileConnectError
-{
-    ContactLookup(ContactLookupError),
-    Connect(AddressConnectError),
+    LookupFailed(SearchAddressError),
+    ConnectFailed(OpenAddressError),
     Other(Box<std::error::Error>),
 }
 
-fn connect_profile(profile_id: ProfileId) ->
-    Box< Future<Item=TcpStream, Error=ProfileConnectError>>
-{
-    let result = find_contacts(profile_id)
-        .map_err( |e| ProfileConnectError::ContactLookup(e) )
-        .and_then( |contacts| {
-            for contact in contacts
-            {
-            }
-            future::err( ProfileConnectError::ContactLookup(ContactLookupError::TODO) )
-        } );
 
-    Box::new(result)
+
+// TODO for Tor, I2P and similar cases, the TcpStream return type might not work out,
+// maybe we'll need something like (Stream<u8>,Sink<u8>) or something similar
+pub fn open(multiaddr: Multiaddr) ->
+    Box< Future<Item=TcpStream, Error=OpenAddressError> >
+{
+    Box::new( future::err(OpenAddressError::TODO) )
 }
 
+
+type ProfileId = String;
+type ServiceId = String;
+
+pub struct Profile
+{
+    id:         ProfileId,
+// TODO
+//    pub_key:    Vec<u8>,
+//    priv_key:   Vec<u8>,
+//    metadata:   ???,
+}
+
+impl Profile
+{
+    pub fn new(id: ProfileId) -> Self
+        { Self{ id: id } }
+
+    // TODO
+    // pub fn register(profile_server: ???) -> ???
+
+    // TODO
+    // pub fn listen(???) -> ???
+
+    pub fn find_profile( /* TODO what filter criteria should we have here? */ ) ->
+        Box< Future<Item=Profile, Error=SearchProfileError> >
+    {
+        Box::new( future::err(SearchProfileError::TODO) )
+    }
+
+    // TODO should we use Stream<Multiaddr> here instead of Vec<>?
+    pub fn find_contacts(&self) ->
+        Box< Future<Item=Vec<Multiaddr>, Error=SearchAddressError> >
+    {
+        Box::new( future::err(SearchAddressError::TODO) )
+    }
+
+    pub fn open(&self) ->
+        Box< Future<Item=TcpStream, Error=ConnectToProfileError>>
+    {
+        let result = self.find_contacts()
+            .map_err( |e| ConnectToProfileError::LookupFailed(e) )
+            .and_then( |contacts|
+            {
+                for contact in contacts
+                {
+                }
+                future::err( ConnectToProfileError::LookupFailed(SearchAddressError::TODO) )
+            } );
+
+        Box::new(result)
+    }
+}
+
+
+
+// TODO
+pub enum ErrorToBeSpecified { TODO, }
+
+pub trait IdentityStore
+{
+    fn connect(addr: Multiaddr) ->
+        Box< Future<Item=Self, Error=ErrorToBeSpecified> >;
+
+    fn register(&self, prof: Profile) ->
+        Box< Future<Item=Profile, Error=ErrorToBeSpecified> >;
+
+    fn remove(&self, prof: Profile) ->
+        Box< Future<Item=(), Error=ErrorToBeSpecified> >;
+}
+
+
+pub trait HomeConnection
+{
+    fn connect(addr: Multiaddr, profiles: Vec<Profile>, services: Vec<ServiceId>) ->
+        Box< Future<Item=Self, Error=ErrorToBeSpecified> >;
+
+    fn stream(&self, srv: ServiceId) -> TcpStream;
+
+    fn register(&self, services: Vec<ServiceId>) ->
+        Box< Future<Item=(), Error=ErrorToBeSpecified> >;
+
+    fn remove(&self, services: Vec<ServiceId>) ->
+        Box< Future<Item=(), Error=ErrorToBeSpecified> >;
+
+    fn close(&self);
+}
 
 
 #[cfg(test)]
 mod tests
 {
     #[test]
-    fn it_works()
+    fn test_register_profile()
+    {
+        // TODO
+    }
+
+    #[test]
+    fn test_lookup_profiles()
+    {
+        // TODO
+    }
+
+    #[test]
+    fn test_lookup_contacts()
+    {
+        // TODO
+    }
+
+    #[test]
+    fn test_connect_multiaddr()
+    {
+        // TODO
+    }
+
+    #[test]
+    fn test_connect_profile()
+    {
+        // TODO
+    }
+
+    #[test]
+    fn test_listen()
     {
         // TODO
     }
