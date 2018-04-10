39c6a6bbe2c754f0e6c711bcc4e60d0ce3752a0a
Rache Bartmoss <bartmoss@tutanota.com>
2018-04-10 13:48:19 +0200

    Temporarily gave up with select_ok() compile problems

--- test/src/lib.rs
+++ test/src/lib.rs
@@ -15,11 +15,11 @@ fn test_events()
     use std::net::ToSocketAddrs;
     use std::rc::Rc;
 
-    use futures::{Stream};
+    use futures::{select_ok, Future, Stream};
     use tokio_core::net::{TcpListener, TcpStream};
     use tokio_core::reactor;
 
-    //use mercury_home_protocol::*;
+    use mercury_home_protocol::*;
     use mercury_connect::HomeContext;
     use mercury_connect::mock::{DummyHome, Signo, make_home_profile};
     use mercury_connect::protocol_capnp::HomeClientCapnProto;
@@ -43,14 +43,25 @@ fn test_events()
         Ok( () )
     } );
 
-    let tcp_fut = TcpStream::connect( &addr, &reactor.handle() );
-    let tcp_stream = reactor.run(tcp_fut).unwrap();
+    let handle2 = reactor.handle();
+    let client_fut = TcpStream::connect( &addr, &reactor.handle() )
+        .map_err( |_e| ErrorToBeSpecified::TODO)
+        .and_then( |tcp_stream|
+        {
+            let signer = Rc::new( Signo::new("privatekey") );
+            let my_profile = signer.prof_id().clone();
+            let home_profile = make_home_profile("home_address", "home_profile", "home_public_key");
+            let home_ctx = Box::new( HomeContext::new(signer, &home_profile) );
+            let client = HomeClientCapnProto::new_tcp( tcp_stream, home_ctx, handle2 );
+            client.login(my_profile) // TODO maybe we should require only a reference in login()
+        } )
+        .map( |session|
+        {
+            session.events() //.for_each( |event| () )
+        } );
 
-    let signer = Rc::new( Signo::new("privatekey") );
-    let home_profile = make_home_profile("home_address", "home_profile", "home_public_key");
-    let home_ctx = Box::new( HomeContext::new(signer, &home_profile) );
-    let client = HomeClientCapnProto::new_tcp( tcp_stream, home_ctx, reactor.handle() );
-
-//    let res = reactor.run(home_session);
-    assert!(true);
+    let futs = [ Box::new( server_fut.map_err( |_e| () ) ) as Box< Future<Item=(),Error=()> >,
+                 Box::new( client_fut.map( |_session| () ).map_err( |_e| () ) ) ];
+//    let both_fut = select_ok( futs.iter() ); // **i as &Future<Item=(),Error=()> ) );
+//    let result = reactor.run(both_fut);
 }
