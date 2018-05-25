75e41b256c0f24d8c3f5f6e8a95a1f60ac14bda8
deusz <lovaszoltanr@gmail.com>
2018-05-25 15:08:34 +0200

    working "filesystem manager", syncronous version

--- storage/src/filesys/mod.rs
+++ storage/src/filesys/mod.rs
@@ -6,6 +6,7 @@ use std::path::Path;
 use std::fs::*;
 use std::collections::HashMap;
 use std::io::{Read, Write};
+use tokio_core::reactor;
 
 pub struct Medusa{
     path : String,
@@ -40,9 +41,9 @@ impl Medusa{
         }
     }
     
-    pub fn new_file_with_name(&self, path : String, file_name : String) -> Result<(), StorageError>{
+    pub fn new_file_with_name(&self, file_path : String, file_name : String) -> Result<(), StorageError>{
         let mut subpath = String::from(self.path.clone());
-        subpath.push_str(&path);
+        subpath.push_str(&file_path);
         subpath.push_str(&file_name);
         match File::create(Path::new(&subpath)){
             Ok(_)=>Ok(()),
@@ -51,15 +52,18 @@ impl Medusa{
     }
 
 
-    pub fn write_to_file(&self, path : String, content : String) -> Result<(), StorageError>{
+    pub fn write_to_file(&self, file_path : String, content : String) -> Result<(), StorageError>{
         let mut subpath = String::from(self.path.clone());
-        subpath.push_str(&path);
-        match File::open(Path::new(&subpath)){
+        subpath.push_str(&file_path);
+        match File::create(Path::new(&subpath)){
             Ok(mut file)=>{
                 match file.write_all(content.as_bytes()){
-                    Ok(_) => Ok(()),
+                    Ok(_) => {
+                        file.sync_all();
+                        Ok(())
+                        },
                     Err(e) => Err(StorageError::Other(Box::new(e)))
-                }
+                } 
             },
             Err(e)=>Err(StorageError::Other(Box::new(e)))
         }
@@ -78,6 +82,12 @@ impl Medusa{
             },
             Err(e)=>Err(StorageError::Other(Box::new(e)))
         }
+    }
+
+    pub fn get_path(&self, file_path: String)->String{
+        let mut path = String::from(self.path.clone());
+        path.push_str(&file_path);
+        path
     }
 }
 
@@ -99,14 +109,14 @@ impl Medusa{
 impl KeyValueStore<String, String> for Medusa{
     fn set(&mut self, key: String, value: String)
         -> Box< Future<Item=(), Error=StorageError> >{
-        let mut path = String::from(self.path.clone());
-        path.push_str(&key);
-        let file_path = Path::new(&path);
-        if !file_path.exists(){
-            if self.new_file(path.clone()).is_err(){
-                return Box::new(future::err(StorageError::OutOfDiskSpace));
-            }
-        }
+        // let mut path = String::new();
+        // path.push_str(&key);
+        // let file_path = Path::new(&path);
+        // if !file_path.exists(){
+        //     if self.new_file(path.clone()).is_err(){
+        //         return Box::new(future::err(StorageError::OutOfDiskSpace));
+        //     }
+        // }
         match self.write_to_file(key, value){
             Ok(_)=>Box::new(future::ok(())),
             Err(e)=>Box::new(future::err(e))
@@ -115,8 +125,7 @@ impl KeyValueStore<String, String> for Medusa{
 
     fn get(&self, key: String)
         -> Box< Future<Item=String, Error=StorageError> >{
-        let mut path = String::from(self.path.clone());
-        path.push_str(&key);
+        let path = self.get_path(key.clone());
         let file_path = Path::new(&path);
         if !file_path.exists(){
             return Box::new(future::err(StorageError::InvalidKey));
@@ -130,6 +139,12 @@ impl KeyValueStore<String, String> for Medusa{
 
 #[test]
 fn medusa_key_value() {
-    let storage = Medusa::init("/ipfs/")?;
-    // storage.
+    let mut reactor = reactor::Core::new().unwrap();
+    println!("\n\n\n");
+    let mut storage : Medusa = Medusa::init(String::from("./ipfs/banan/")).unwrap();
+    let json = String::from("<Json:json>");
+    reactor.run(storage.set(String::from("alma.json"), json.clone())).unwrap();
+    let read = storage.get(String::from("alma.json"));
+    let res = reactor.run(read).unwrap();
+    assert_eq!(res, json);
 }
