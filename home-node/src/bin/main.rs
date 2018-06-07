6ffb6686d3169fc409e35fca70806de5142d6f30
Rache Bartmoss <bartmoss@tutanota.com>
2018-06-07 14:48:28 +0200

    Added log4rs into server

--- home-node/src/bin/main.rs
+++ home-node/src/bin/main.rs
@@ -1,6 +1,9 @@
 extern crate capnp;
 extern crate capnp_rpc;
 extern crate futures;
+#[macro_use]
+extern crate log;
+extern crate log4rs;
 extern crate mercury_home_protocol;
 extern crate mercury_home_node;
 extern crate mercury_storage;
@@ -16,7 +19,7 @@ use std::rc::Rc;
 use futures::Stream;
 use tokio_core::{reactor, net::TcpListener};
 
-use mercury_home_protocol::{crypto::*, *};
+use mercury_home_protocol::{*, crypto::*};
 use mercury_home_node::{server::*, protocol_capnp};
 use mercury_storage::async::imp::InMemoryStore;
 
@@ -24,6 +27,8 @@ use mercury_storage::async::imp::InMemoryStore;
 
 fn main()
 {
+    log4rs::init_file( "log4rs.yml", Default::default() ).unwrap();
+
     // TODO parse a configuration and set up keys accordingly, possibly from hardware wallet
     let secret_key = PrivateKey( b"\x83\x3F\xE6\x24\x09\x23\x7B\x9D\x62\xEC\x77\x58\x75\x20\x91\x1E\x9A\x75\x9C\xEC\x1D\x19\x75\x5B\x7D\xA9\x01\xB9\x6D\xCA\x3D\x42".to_vec() );
     let public_key = PublicKey( b"\xEC\x17\x2B\x93\xAD\x5E\x56\x3B\xF4\x93\x2C\x70\xE1\x24\x50\x34\xC3\x54\x67\xEF\x2E\xFD\x4D\x64\xEB\xF8\x19\x68\x34\x67\xE2\xBF".to_vec() );
@@ -46,10 +51,10 @@ fn main()
     let addr = "localhost:9876".to_socket_addrs().unwrap().next().expect("Failed to parse address");
     let socket = TcpListener::bind(&addr, &handle).expect("Failed to bind socket");
 
-    println!("Waiting for clients");
+    debug!("Server started, waiting for clients");
     let done = socket.incoming().for_each(move |(socket, _addr)|
     {
-        println!("Accepted client connection, serving requests");
+        debug!("Accepted client connection, serving requests");
 
         // TODO fill this in properly for each connection based on TLS authentication info
         let client_pub_key = PublicKey( b"TestPublicKey: TODO implement filling this in properly with TLS authentication info".to_vec() );
