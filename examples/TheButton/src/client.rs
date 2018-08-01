c1f07a1c300147033eb9353afa0affb2e6ef04a1
Andras Szabo <andrei.hu@gmail.com>
2018-08-01 12:02:45 +0200

    client updates

--- examples/TheButton/src/client.rs
+++ examples/TheButton/src/client.rs
@@ -7,6 +7,10 @@ pub struct Client{
     mercury: Box<DAppApi>
 }
 
+enum ClientState {
+    Connecting
+}
+
 impl Client{
     pub fn new(cfg: ClientConfig, appctx: AppContext) -> Self{
         unimplemented!();
@@ -19,25 +23,14 @@ impl Client{
         */
     }
 
-    pub fn run(&self)->i32{
-        unimplemented!();
-    }
-
-    fn connect(&self)->bool{
-        unimplemented!()
-    }
-
-    fn on_event(){
-        panic!("TODO on event");
-    }
 }
 
 impl Future for Client{
     type Item = i32;
     type Error = std::io::Error;
-    fn poll(&mut self) ->
-        std::result::Result<futures::Async<<Self as futures::Future>::Item>, <Self as futures::Future>::Error>{
-            unimplemented!();
-            
+    fn poll(&mut self) -> std::result::Result<futures::Async<<Self as futures::Future>::Item>, <Self as futures::Future>::Error>
+    {
+
+                
     }
 }
