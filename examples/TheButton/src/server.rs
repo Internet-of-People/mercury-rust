8cd4a3007549991e14e7c2fcf3985bd89aebea72
Andras Szabo <andrei.hu@gmail.com>
2018-08-02 17:09:14 +0200

    cleaned up source file a bit by removing comments

--- examples/TheButton/src/server.rs
+++ examples/TheButton/src/server.rs
@@ -50,19 +50,6 @@ impl Server{
             cfg : cfg,
         }
     }
-/*
-    pub fn generate_event(&mut self)-> bool {
-        info!("generating event {} {}", self.event_stock, self.sent);
-
-        // iterate connected clients and send them a message
-
-        self.sent += 1;
-        match self.cfg.event_count {
-            Some(limit) => limit == self.sent,
-            _ => false    
-        }
-    }
-*/
     // fn handle_event_file(file_name: String)->u32{
     //     let mut path = String::from("\0");
     //     path.push_str(&file_name);
@@ -82,92 +69,6 @@ impl Server{
     //         }).then(|_| Ok(()));
     //     tokio::run(uds_incoming);
     // }
-
-    //TODO this is unusable right now, and so no multi line socketing will work
-    // fn read_some(stream : UnixStream, buf : Vec<u8>){
-    //     read(stream, buf)
-    //         .map(|(stream, buf, byte)|{
-    //             for _ in 0..byte-1{
-    //                 Self::generate_event();
-    //             }
-    //         Self::read_some(stream, buf)
-    //     });
-    // }
-
-    pub fn stop_event_generation(){
-        info!("Stopped event auto-generation");
-    }   
-
-/*
-    fn handle_sigint(&mut self) -> Option<futures::Poll<i32, std::io::Error>>{
-        match self.sig.poll(){
-            Ok(Async::Ready(Some(_)))=>{
-                debug!("SIGINT, closing");
-                return Some(Err(std::io::Error::new(std::io::ErrorKind::Other,"SIGINT received")));    
-            }
-            Ok(Async::NotReady)=>{
-                return None;               
-            }
-            Ok(Async::Ready(None))=>{
-                info!("SIGINT stream closed");
-                return Some(Err(std::io::Error::new(std::io::ErrorKind::Other,"SIGINT stream closed")));
-            }
-            Err(e)=>{
-                warn!("SIGINT error");
-                return Some(Err(std::io::Error::new(std::io::ErrorKind::Other,e.description())));
-            }   
-        }
-    }
-    fn handle_sigusr1(&mut self) -> Option<futures::Poll<i32, std::io::Error>>{
-        match self.usr1.poll(){
-            Ok(Async::Ready(Some(_)))=>{
-                match self.cfg.event_count{
-                    Some(c)=>{
-                        if self.sent>=c{
-                            return Some(Err(std::io::Error::new(std::io::ErrorKind::Other, "event limit reached"))); 
-                        }
-                        self.sent += 1;
-                    }
-                    None=>{();}
-                }
-                // for call in self.calls{
-                //     self.generate_event(&call);
-                // }
-                return None;    
-            }
-            Ok(Async::NotReady)=>{
-                return None;             
-            }
-            Ok(Async::Ready(None))=>{
-                info!("SIGUSR1 stream closed");
-                return Some(Err(std::io::Error::new(std::io::ErrorKind::Other,"SIGUSR1 stream closed")));
-            }
-            Err(e)=>{
-                warn!("SIGUSR1 error");
-                return Some(Err(std::io::Error::new(std::io::ErrorKind::Other,e.description())));
-            }   
-        }
-    }
-    fn handle_sigusr2(&mut self) -> Option<futures::Poll<i32, std::io::Error>>{
-        match self.usr2.poll(){
-            Ok(Async::Ready(Some(_)))=>{
-                self.del = None;
-                return None;    
-            }
-            Ok(Async::NotReady)=>{
-                return None;              
-            }
-            Ok(Async::Ready(None))=>{
-                info!("SIGUSR2 stream closed");
-                return Some(Err(std::io::Error::new(std::io::ErrorKind::Other,"SIGUSR2 stream closed")));
-            }
-            Err(e)=>{
-                warn!("SIGUSR2 error");
-                return Some(Err(std::io::Error::new(std::io::ErrorKind::Other,e.description())));
-            }   
-        }
-    }
-    */
 }
 
 impl IntoFuture for Server {
@@ -223,142 +124,15 @@ impl IntoFuture for Server {
             tx_sigint1.clone().send(()).map(|_| ()).map_err(|err |std::io::Error::new(std::io::ErrorKind::Other, err))
         });
 
-
-
-        let fut = sigint_fut.select(sigusr1_fut).map(|(item, _)| item).map_err(|(err, _)| err);
+        // SIGUSR1 is generating an event
+        let sigusr2_fut = signal_recv(SIGUSR2).for_each(move |_| {
+            info!("received SIGUSR2, toggling automatic event generation");            
+            Ok(())
+        });
         
+        let fut = sigint_fut.select(sigusr1_fut).map(|(item, _)| item).map_err(|(err, _)| err);        
         Box::new(fut)
     }
 }
 
 
-/*
-impl Future for Server{
-    type Item = ();
-    type Error = std::io::Error;
-    fn poll(&mut self) -> futures::Poll<Self::Item, Self::Error>{
-        match self.handle_sigint(){
-            Some(err) => return err,
-            None => ()
-        }
-        match self.handle_sigusr1(){
-            Some(err) => return err,
-            None => ()
-        }
-        match self.handle_sigusr2(){
-            Some(err) => return err,
-            None => ()
-        }
-
-        match self.callstream.poll(){
-            Ok(Async::Ready(Some(call)))=>{
-                debug!("generate_event");
-                // self.calls.push(call);
-                match call {
-                    Ok(c) =>{ 
-                        match c.request_details().to_caller{
-                            Some(sink)=>{
-                                self.calls.push(sink.to_owned());
-                            },
-                            _=>(),
-                        }
-                    },//handling of incoming calls is problematic
-                    Err(e) =>{ return Ok(Async::NotReady);},
-                }
-                
-                // Ok(Async::NotReady)    
-            }
-            Ok(Async::NotReady)=>{
-                debug!("not ready");
-                return Ok(Async::NotReady);                    
-            }
-            Ok(Async::Ready(None))=>{
-                debug!("stream close");
-                return Err(std::io::Error::new(std::io::ErrorKind::Other,"timer failed"));
-            }
-            Err(e)=>{
-                debug!("error");
-                return Err(std::io::Error::new(std::io::ErrorKind::Other,"TODOe.description()"));
-            }
-        }
-
-        match self.cfg.event_count{
-            Some(c)=>{
-                while self.event_stock > 0 {
-                    if self.sent >= c {
-                        return Err(std::io::Error::new(std::io::ErrorKind::Other, "event limit reached"));
-                    }   
-                    self.event_stock-=1;
-                    self.sent+=1;
-                    for call in self.calls{
-                        self.generate_event(&call);
-                    }
-                }
-                ();
-            },
-            None=>{();}
-        }
-        match self.uds{
-            Some(ref mut incoming)=>{
-                let mut gen : u16 = 0;
-                match incoming.poll(){
-                    Ok(Async::Ready(Some(sock)))=>{
-                        let mut s : Vec<u8> = Vec::new();
-                        s.resize(10, 1);
-                        warn!("whatever");
-                        match read(sock, s).poll(){
-                            Ok(Async::Ready((_stream, _buf, byte)))=>{
-                                gen = byte as u16;
-                            }
-                            _=>{}
-                        }
-                    }
-                    Ok(Async::NotReady)=>{
-                        ()                 
-                    }
-                    Ok(Async::Ready(None))=>{
-                        debug!("stream close");
-                        return Err(std::io::Error::new(std::io::ErrorKind::Other,"timer failed"));
-                    }
-                    Err(e)=>{
-                        debug!("error");
-                        return Err(std::io::Error::new(std::io::ErrorKind::Other,e.description()));
-                    }
-                }
-                self.event_stock += gen;
-            },
-            _ => ()
-        }
-
-        match self.del{
-            Some(ref mut intval)=>{
-                loop{
-                    match intval.poll(){
-                        Ok(Async::Ready(Some(_)))=>{
-                            debug!("generate_event");
-                            self.event_stock += 1;
-                            // Ok(Async::NotReady)    
-                        }
-                        Ok(Async::NotReady)=>{
-                            debug!("not ready");
-                            return Ok(Async::NotReady);                    
-                        }
-                        Ok(Async::Ready(None))=>{
-                            debug!("stream close");
-                            return Err(std::io::Error::new(std::io::ErrorKind::Other,"timer failed"));
-                        }
-                        Err(e)=>{
-                            debug!("error");
-                            return Err(std::io::Error::new(std::io::ErrorKind::Other,e.description()));
-                        }
-                    }
-                }
-            }
-            None=>{
-                Ok(Async::NotReady)
-            }
-        }
-        
-    }
-}
-*/
