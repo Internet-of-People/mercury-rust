63fd5686cec165964dbcdc4c8eacd03078c04bf9
Rache Bartmoss <bartmoss@tutanota.com>
2019-01-16 17:13:40 +0100

    Fixed type

--- src/cli.rs
+++ src/cli.rs
@@ -39,7 +39,7 @@ pub enum Command
     /// Set attribute
     Set(SetCommand),
 
-    #[structopt(name = "Clear")]
+    #[structopt(name = "clear")]
     /// Clear attribute
     Clear(ClearCommand),
 }
