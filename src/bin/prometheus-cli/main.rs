e9c393f4fe51b8e992cf9dd039f7f30497c48a8a
Rache Bartmoss <bartmoss@tutanota.com>
2019-01-28 09:57:54 +0100

    Arranged basic types between morpheus-storage and prometheus, initial command handling logic attempt

--- src/bin/prometheus-cli/main.rs
+++ src/bin/prometheus-cli/main.rs
@@ -2,6 +2,7 @@ mod cli;
 
 
 
+use prometheus::vault::ProfileVault;
 use crate::cli::*;
 
 
@@ -12,17 +13,19 @@ fn main() -> Result<(), &'static str>
     let command = Command::from_args();
     println!("{:?}", command);
 
-//    let keyvault = KeyVault{};
-//    match command {
-//        Command::Create(CreateCommand::Link{my_profile_id, peer_profile_id}) => {
-//            let my_profile = match my_profile_id.or( keyvault.get_active() ) {
-//                Some(id) => id,
-//                None => return Err("Command option my_profile_id is unspecified and no active default profile was found"),
-//            };
-//            let signer = keyvault.get(&my_profile);
-//        },
-//        _ => {},
-//    };
+    let vault = ProfileVault{};
+    match command {
+        Command::Create(CreateCommand::Link{my_profile_id, peer_profile_id}) => {
+            let my_profile_id = match my_profile_id.or( vault.get_active_profile() ) {
+                Some(id) => id,
+                None => return Err("Command option my_profile_id is unspecified and no active default profile was found"),
+            };
+            let mut profile = vault.get_profile(&my_profile_id);
+            let link = profile.create_link(&peer_profile_id);
+            println!("Created link: {:?}", link);
+        },
+        _ => {},
+    };
 
     Ok( () )
 }
--- src/vault.rs
+++ src/vault.rs
@@ -1,29 +1,20 @@
 //use std::collections::HashMap;
-//use std::rc::Rc;
+use std::rc::Rc;
 
 use morpheus_storage::*;
+use crate::types::{Link, PublicKey, Signature};
 
 
 
-pub struct Link
+pub trait Signer
 {
-    pub id: LinkId,
-//    pub source_profile: ProfileId, // NOTE this might be needed when serialized, but redundant when in-memory
-    pub peer_profile: ProfileId,
-//    pub metadata: HashMap<AttributeId,AttributeValue>,
+    fn profile_id(&self) -> &ProfileId;
+    fn public_key(&self) -> &PublicKey;
+    fn sign(&self, data: &[u8]) -> Signature;
+    //fn encrypt(&self, data: &[u8], target: &PublicKey) -> Vec<u8>;
 }
 
 
-
-//pub trait Signer
-//{
-//    fn profile_id(&self) -> &ProfileId;
-//    fn public_key(&self) -> &PublicKey;
-//    fn sign(&self, data: &[u8]) -> Signature;
-//    //fn encrypt(&self, data: &[u8], target: &PublicKey) -> Vec<u8>;
-//}
-//
-//
 //pub struct KeyVault {}
 //impl KeyVault
 //{
@@ -37,14 +28,15 @@ pub struct Link
 
 
 
-//pub trait ProfileVault
-//{
-//    fn list_profiles() -> Vec<ProfileId>;
-//    fn get_profile(id: &ProfileId) -> Box<Profile>; // TODO or should list_profiles() return Vec<Profile> and drop this function?
-//    fn create_profile() -> Box<Profile>;
-//    // TODO what does this mean? Purge related metadata from local storage?
-//    // fn remove_profile(id: &ProfileId);
-//
-//    fn get_active_profile() -> Option<ProfileId>;
-//    fn set_active_profile(id: &ProfileId);
-//}
+pub struct ProfileVault;
+impl ProfileVault
+{
+    pub fn list_profiles(&self) -> Vec<ProfileId> { unimplemented!() }
+    pub fn get_profile(&self, id: &ProfileId) -> Box<Profile> { unimplemented!() } // TODO or should list_profiles() return Vec<Profile> and drop this function?
+    pub fn create_profile(&self) -> Box<Profile> { unimplemented!() }
+    // TODO what does this mean? Purge related metadata from local storage?
+    // fn remove_profile(id: &ProfileId);
+
+    pub fn get_active_profile(&self) -> Option<ProfileId> { unimplemented!() }
+    pub fn set_active_profile(&self, id: &ProfileId) { unimplemented!() }
+}
