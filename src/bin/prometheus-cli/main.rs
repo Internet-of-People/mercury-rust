e25cc1e588d949f9b25547ee3f0a6426d63736d1
Rache Bartmoss <bartmoss@tutanota.com>
2019-01-31 11:18:31 +0100

    Added logging, Started working on network connection to storage

--- src/bin/prometheus-cli/main.rs
+++ src/bin/prometheus-cli/main.rs
@@ -1,9 +1,11 @@
+//use std::net::SocketAddr;
 use std::sync::{Arc, RwLock};
+use std::time::Duration;
 
 use log::*;
 
 use prometheus::types::*;
-use prometheus::vault::ProfileVault;
+use prometheus::vault::*;
 use crate::cli::*;
 
 
@@ -14,17 +16,27 @@ mod cli;
 
 fn main() -> Result<(), &'static str>
 {
+    // TODO fix all these unwraps with proper error handling
+    log4rs::init_file( "log4rs.yml", Default::default() ).unwrap();
+
     use structopt::StructOpt;
     let command = Command::from_args();
     info!("Got command {:?}", command);
 
-    process_command(command)
+//    let addr = "127.0.0.1:1234".parse().unwrap();
+//    let timeout = Duration::from_secs(5);
+//    info!("Initializing profile vault, connecting to {:?}", addr);
+//    let vault = DummyProfileVault::new(&addr, timeout).unwrap();
+
+    let vault = FailingProfileVault{};
+
+    process_command(command, &vault)
 }
 
 
 
 fn selected_profile(vault: &ProfileVault, my_profile_option: Option<ProfileId>)
-    -> Result<Arc<RwLock<Profile>>, &'static str>
+                    -> Result<Arc<RwLock<Profile>>, &'static str>
 {
     let profile_opt = my_profile_option.or( vault.get_active() )
         .and_then( |profile_id| vault.get(&profile_id) );
@@ -47,13 +59,12 @@ where F: FnOnce(&mut Profile) -> ()
 }
 
 
-fn process_command(command: Command) -> Result<(), &'static str>
+fn process_command(command: Command, vault: &ProfileVault) -> Result<(), &'static str>
 {
-    let vault = ProfileVault{};
     match command
     {
         Command::Create(CreateCommand::Link{my_profile_id, peer_profile_id}) => {
-            on_profile(&vault, my_profile_id, |profile| {
+            on_profile(vault, my_profile_id, |profile| {
                 let link = profile.create_link(&peer_profile_id);
                 info!("Created link: {:?}", link);
             } )?;
@@ -63,20 +74,20 @@ fn process_command(command: Command) -> Result<(), &'static str>
             let created_profile_ptr = vault.create();
             let created_profile = match created_profile_ptr.read() {
                 Ok(profile) => profile,
-                Err(e) => return Err("Implementation error: failed to read created profile"),
+                Err(_e) => return Err("Implementation error: failed to read created profile"),
             };
             info!( "Created profile with id {}", created_profile.id() );
         },
 
         Command::Clear(ClearCommand::Attribute{my_profile_id, key}) => {
-            on_profile(&vault, my_profile_id, |profile| {
+            on_profile(vault, my_profile_id, |profile| {
                 profile.clear_attribute(&key);
                 info!("Cleared attribute: {:?}", key);
             } )?;
         },
 
         Command::List(ListCommand::IncomingLinks{my_profile_id}) => {
-            on_profile(&vault, my_profile_id, |profile| {
+            on_profile(vault, my_profile_id, |profile| {
                 let followers = profile.followers();
                 for follower in followers {
                     info!("  Follower: {:?}", follower);
@@ -89,7 +100,7 @@ fn process_command(command: Command) -> Result<(), &'static str>
         },
 
         Command::Remove(RemoveCommand::Link{my_profile_id, link_id}) => {
-            on_profile(&vault, my_profile_id, |profile| {
+            on_profile(vault, my_profile_id, |profile| {
                 profile.remove_link(&link_id);
                 info!("Removed link: {:?}", link_id);
             } )?;
@@ -101,7 +112,7 @@ fn process_command(command: Command) -> Result<(), &'static str>
         },
 
         Command::Set(SetCommand::Attribute{my_profile_id, key, value}) => {
-            on_profile(&vault, my_profile_id, |profile| {
+            on_profile(vault, my_profile_id, |profile| {
                 info!("Setting attribute {} to {}", key, value);
                 profile.set_attribute(key, value);
             } )?;
--- src/vault.rs
+++ src/vault.rs
@@ -1,42 +1,68 @@
 //use std::collections::HashMap;
+use std::net::{SocketAddr, TcpStream};
 use std::sync::{Arc, RwLock};
+use std::time::Duration;
 
+//use morpheus_keyvault::*;
 use morpheus_storage::*;
 //use crate::types::{Link, PublicKey, Signature};
 
 
 
-//pub trait Signer
-//{
-//    fn profile_id(&self) -> &ProfileId;
-//    fn public_key(&self) -> &PublicKey;
-//    fn sign(&self, data: &[u8]) -> Signature;
-//    //fn encrypt(&self, data: &[u8], target: &PublicKey) -> Vec<u8>;
-//}
-//
-//
-//pub struct KeyVault {}
-//impl<K: KeyDerivationCrypto> KeyVault
-//{
-//    pub fn list(&self) -> Vec<ProfileId> { unimplemented!() }
-//    pub fn get_public(&self, _profile_id: &ProfileId) -> PublicKey { unimplemented!() }
-//    pub fn get(&self, _profile_id: &ProfileId, authentication: TODO) -> Rc<Signer> { unimplemented!() }
-//    pub fn create(&mut self) -> Rc<Signer> { unimplemented!() }
-//}
-
-
-
 // TODO several functions should return Result<> instead, but that needs building our own proper error data structures
-pub struct ProfileVault;
-impl ProfileVault
+pub trait ProfileVault
 {
-    pub fn list(&self) -> Vec<ProfileId> { unimplemented!() } // TODO should this return an iterator instead?
-    pub fn get(&self, id: &ProfileId) -> Option< Arc<RwLock<Profile>> > { unimplemented!() } // TODO or should list_profiles() return Vec<Profile> and drop this function?
-    pub fn create(&self) -> Arc<RwLock<Profile>> { unimplemented!() }
+    fn list(&self) -> Vec<ProfileId>; // TODO should this return an iterator instead?
+    fn get(&self, id: &ProfileId) -> Option< Arc<RwLock<Profile>> >; // TODO or should list_profiles() return Vec<Profile> and drop this function?
+    fn create(&self) -> Arc<RwLock<Profile>>;
     // TODO what does this mean? Purge related metadata from local storage plus don't show it in the list,
     //      or maybe also delete all links/follows with other profiles
+    fn remove(&self, id: &ProfileId);
+
+    fn get_active(&self) -> Option<ProfileId>;
+    fn set_active(&self, id: &ProfileId) -> bool;
+}
+
+
+
+pub struct DummyProfileVault
+{
+    profile: Arc<RwLock<Profile>>,
+}
+
+impl DummyProfileVault
+{
+    pub fn new(addr: &SocketAddr, timeout: Duration) -> std::io::Result<Self>
+    {
+        let tcp_stream = TcpStream::connect_timeout(addr, timeout)?;
+        let profile = RpcProfile::new(tcp_stream);
+        Ok( Self{ profile: Arc::new( RwLock::new(profile) ) } )
+    }
+}
+
+impl ProfileVault for DummyProfileVault
+{
+    fn list(&self) -> Vec<ProfileId> { unimplemented!() }
+    fn get(&self, id: &ProfileId) -> Option< Arc<RwLock<Profile>> > { unimplemented!() }
+    fn create(&self) -> Arc<RwLock<Profile>> { unimplemented!() }
     fn remove(&self, id: &ProfileId) { unimplemented!() }
 
-    pub fn get_active(&self) -> Option<ProfileId> { unimplemented!() }
-    pub fn set_active(&self, id: &ProfileId) -> bool { unimplemented!() }
+    fn get_active(&self) -> Option<ProfileId> { unimplemented!() }
+    fn set_active(&self, id: &ProfileId) -> bool { unimplemented!() }
+}
+
+
+
+// TODO remove this when something else works and can be tested
+pub struct FailingProfileVault {}
+
+impl ProfileVault for FailingProfileVault
+{
+    fn list(&self) -> Vec<ProfileId> { unimplemented!() }
+    fn get(&self, id: &ProfileId) -> Option< Arc<RwLock<Profile>> > { unimplemented!() }
+    fn create(&self) -> Arc<RwLock<Profile>> { unimplemented!() }
+    fn remove(&self, id: &ProfileId) { unimplemented!() }
+
+    fn get_active(&self) -> Option<ProfileId> { unimplemented!() }
+    fn set_active(&self, id: &ProfileId) -> bool { unimplemented!() }
 }
