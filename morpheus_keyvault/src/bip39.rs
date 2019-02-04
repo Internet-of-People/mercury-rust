fe49c22788adc8f2e18814903219ea79b5b142db
wigy <wigy_opensource_developer@yahoo.com>
2019-02-04 20:47:38 +0100

    Using bip39-rs v0.6-beta.1 with fixed seed generation

--- morpheus_keyvault/src/bip39.rs
+++ morpheus_keyvault/src/bip39.rs
@@ -1,27 +1,17 @@
-use std::sync::Mutex;
 use std::borrow::Borrow;
-use bip39::{Mnemonic, MnemonicType, Language};
+use bip39::{Mnemonic, MnemonicType, Language, Seed};
 use failure::Fallible;
 
-// Simple glue code between `error-chain` used by bip39-rs 0.5.1 and `failure` used by us.
-// Fail implementations needs to be Sync and Mutex can achieve that.
-#[derive(Debug, Fail)]
-#[fail(display = "bip39-rs: {:#?}", e)]
-pub(crate) struct Bip39Error {
-    e: Mutex<bip39::Error>,
-}
-
-fn as_fallible_vec(mnemonic_res: Result<Mnemonic, bip39::Error>) -> Fallible<Vec<u8>> {
+fn as_fallible_vec(mnemonic_res: Fallible<Mnemonic>, password: &str) -> Fallible<Vec<u8>> {
     mnemonic_res
-        .map(|m| m.as_seed().as_bytes().to_owned() )
-        .map_err(|e| Bip39Error { e: Mutex::from(e) }.into() )
+        .map(|m| Seed::new(&m, password).as_bytes().to_owned() )
 }
 
 pub(crate) fn generate_new(password: &str) -> Fallible<Vec<u8>> {
-    as_fallible_vec(Mnemonic::new(MnemonicType::Type24Words, Language::English, password.to_owned()))
+    as_fallible_vec(Ok(Mnemonic::new(MnemonicType::Words24, Language::English)), password)
 }
 
 pub(crate) fn from_phrase<T: Borrow<str>>(words: &[T], password: &str) -> Fallible<Vec<u8>> {
     let phrase = words.join(" ");
-    as_fallible_vec(Mnemonic::from_string(phrase, Language::English, password.to_owned()))
+    as_fallible_vec(Mnemonic::from_phrase(phrase, Language::English), password)
 }
