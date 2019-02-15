97029f31a7ac02c652b1e0f6b5d84b40918431d2
Rache Bartmoss <bartmoss@tutanota.com>
2019-02-15 13:37:38 +0100

    added platform-independent detecion of app config dir

--- prometheus/src/bin/prometheus-cli/main.rs
+++ prometheus/src/bin/prometheus-cli/main.rs
@@ -1,6 +1,10 @@
-use failure::Fallible;
-use log::*;
+use std::path::Path;
 use std::time::Duration;
+
+use dirs;
+use failure::{err_msg, Fallible};
+use log::*;
+use structopt::StructOpt;
 
 use crate::cli::*;
 use morpheus_storage::DummyProfileStore;
@@ -11,7 +15,17 @@ mod cli;
 fn main() -> Fallible<()> {
     log4rs::init_file("log4rs.yml", Default::default())?;
 
-    use structopt::StructOpt;
+    let cfg_dir = dirs::config_dir()
+        .ok_or_else( || err_msg("Failed to detect platform-dependent directory for app config") )?;
+    let config_path = Path::new(&cfg_dir).join("prometheus").join("config");
+    info!("Looking for app state in {:?}", config_path);
+    let config_state = if config_path.exists() {
+        //load()
+    }
+    else {
+
+    };
+
     let command = Command::from_args();
     info!("Got command {:?}", command);
 
