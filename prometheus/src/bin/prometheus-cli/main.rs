a23872829a9a0079bc504a7781b8515d3a27acaa
Rache Bartmoss <bartmoss@tutanota.com>
2019-02-15 14:14:22 +0100

    ProfileVault is generated or loaded on app startup

--- prometheus/src/bin/prometheus-cli/main.rs
+++ prometheus/src/bin/prometheus-cli/main.rs
@@ -17,12 +17,22 @@ fn main() -> Fallible<()> {
 
     let cfg_dir = dirs::config_dir()
         .ok_or_else(|| err_msg("Failed to detect platform-dependent directory for app config"))?;
-    let config_path = Path::new(&cfg_dir).join("prometheus").join("config");
-    info!("Looking for app state in {:?}", config_path);
-    let config_state = if config_path.exists() {
-        //load()
-    } else {
+    let app_cfg_dir = Path::new(&cfg_dir).join("prometheus");
+    let config_path = app_cfg_dir.join("config");
+    info!("Looking for app config in {:?}", config_path);
 
+    let vault = if config_path.exists() {
+        info!("Found config, loading");
+        let cfg_file = std::fs::File::open(&config_path)?;
+        let vault: DummyProfileVault = serde_json::from_reader(&cfg_file)?;
+        vault
+    } else {
+        info!("No config found, generating it");
+        let vault = DummyProfileVault::new();
+        std::fs::create_dir_all(&app_cfg_dir)?;
+        let cfg_file = std::fs::File::create(&config_path)?;
+        serde_json::to_writer_pretty(&cfg_file, &vault)?;
+        vault
     };
 
     let command = Command::from_args();
@@ -30,8 +40,6 @@ fn main() -> Fallible<()> {
 
     let addr = "127.0.0.1:6161".parse()?;
     let timeout = Duration::from_secs(5);
-
-    let vault = DummyProfileVault::new();
     let store = DummyProfileStore::new(&addr, timeout)?;
 
     let mut ctx = CommandContext::new(Box::new(vault), Box::new(store));
