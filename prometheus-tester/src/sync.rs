bb5e2d4d8dddbc259d5cb6063097400c2a8b171d
wigy <wigy_opensource_developer@yahoo.com>
2019-03-05 12:04:50 +0100

    Fix: synchronization creates all profiles first so link creation does not fail

--- prometheus-tester/src/sync.rs
+++ prometheus-tester/src/sync.rs
@@ -8,9 +8,25 @@ use crate::{state::State, vault::Vault};
 pub fn synchronize(state: &mut State, repo: &mut ProfileRepository) -> Fallible<()> {
     let vault = Vault::new(&state.vault_seed())?;
     let mut id_map = std::collections::HashMap::<usize, ProfileId>::with_capacity(state.len());
+
     for (idx, _user) in state.into_iter().enumerate() {
         let id = vault.profile_id(idx)?;
-        id_map.insert(idx, id);
+        id_map.insert(idx, id.clone());
+        let profile = repo
+            .get(&id)
+            .ok_or_else(|| err_msg("Could not connect to server"))?;
+
+        let links_res = profile.clone().borrow().links();
+        match links_res {
+            Ok(_links) => {
+                debug!("Found {}: {}", idx, id);
+            }
+            Err(e) => {
+                debug!("Not found {}: {}", idx, e);
+                repo.create(&id)?;
+                info!("Re-created {}: {}", idx, id);
+            }
+        }
     }
 
     for (idx, user) in state.into_iter().enumerate() {
@@ -22,7 +38,6 @@ pub fn synchronize(state: &mut State, repo: &mut ProfileRepository) -> Fallible<
         let links_res = profile.clone().borrow().links();
         match links_res {
             Ok(links) => {
-                debug!("Found {}: {}", idx, id);
                 for peer in user.into_iter() {
                     let peer_id = &id_map[peer];
                     if links.iter().find(|l| l.peer_profile == *peer_id).is_none() {
@@ -32,14 +47,7 @@ pub fn synchronize(state: &mut State, repo: &mut ProfileRepository) -> Fallible<
                 }
             }
             Err(e) => {
-                debug!("Not found {}: {}", idx, e);
-                let profile = repo.create(id)?;
-                info!("Re-created {}: {}", idx, id);
-                for peer in user.into_iter() {
-                    let peer_id = &id_map[peer];
-                    profile.clone().borrow_mut().create_link(peer_id)?;
-                    info!("Re-created link {}->{}: {}->{}", idx, peer, id, peer_id);
-                }
+                error!("Not found links from just created profile {}: {}", idx, e);
             }
         }
     }
