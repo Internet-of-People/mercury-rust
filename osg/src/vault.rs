0acb3e6ef87e9665225f4b0e582357a2d9a870a6
wigy <wigy_opensource_developer@yahoo.com>
2019-03-13 17:00:12 +0100

    Added ProfileVault::restore_id changing the invariants of HdProfileVault

--- osg/src/vault.rs
+++ osg/src/vault.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::path::PathBuf;
 
-use failure::{bail, Fallible};
+use failure::{bail, ensure, Fallible};
 use log::*;
 use serde_derive::{Deserialize, Serialize};
 
@@ -15,6 +15,7 @@ use keyvault::{
 pub trait ProfileVault {
     fn list(&self) -> Fallible<Vec<ProfileId>>;
     fn create_id(&mut self) -> Fallible<ProfileId>;
+    fn restore_id(&mut self, id: &ProfileId) -> Fallible<()>;
 
     fn get_active(&self) -> Fallible<Option<ProfileId>>;
     fn set_active(&mut self, id: &ProfileId) -> Fallible<()>;
@@ -23,10 +24,12 @@ pub trait ProfileVault {
     fn save(&self, filename: &PathBuf) -> Fallible<()>;
 }
 
+const GAP: i32 = 20;
+
 #[derive(Debug, Deserialize, Serialize)]
 pub struct HdProfileVault {
     pub seed: Seed,
-    pub indexes: Vec<i32>,
+    pub next_idx: i32,
     pub active_idx: Option<i32>,
 }
 
@@ -35,7 +38,7 @@ impl HdProfileVault {
         info!("Initializing new vault");
         Self {
             seed,
-            indexes: Default::default(),
+            next_idx: Default::default(),
             active_idx: Option::None,
         }
     }
@@ -51,11 +54,26 @@ impl HdProfileVault {
         Ok(key_id.into())
     }
 
+    fn index_of(&self, id: &ProfileId) -> Option<usize> {
+        self.list()
+            .ok()
+            .and_then(|v| v.iter().position(|candidate_id| candidate_id == id))
+    }
+
     pub fn load(filename: &PathBuf) -> Fallible<Self> {
         trace!("Loading profile vault from {:?}", filename);
         let vault_file = File::open(filename)?;
         //let vault: Self = serde_json::from_reader(&vault_file)?;
         let vault: Self = bincode::deserialize_from(vault_file)?;
+        ensure!(vault.next_idx >= 0, "next_idx cannot be negative");
+        if let Some(active) = vault.active_idx {
+            ensure!(active >= 0, "active_idx cannot be negative");
+            ensure!(
+                active < vault.next_idx,
+                "active_idx cannot exceed last profile index"
+            );
+        }
+
         Ok(vault)
     }
 }
@@ -63,23 +81,35 @@ impl HdProfileVault {
 impl ProfileVault for HdProfileVault {
     fn list(&self) -> Fallible<Vec<ProfileId>> {
         let mercury_xsk = self.mercury_xsk()?;
-        self.indexes
-            .iter()
-            .try_fold(Vec::with_capacity(self.indexes.len()), |mut v, idx| {
-                let profile_id = Self::profile_id(&mercury_xsk, *idx)?;
-                v.push(profile_id);
-                Ok(v)
-            })
+        let mut v = Vec::with_capacity(self.next_idx as usize);
+        for idx in 0..self.next_idx {
+            let profile_id = Self::profile_id(&mercury_xsk, idx)?;
+            v.push(profile_id);
+        }
+        Ok(v)
     }
 
     fn create_id(&mut self) -> Fallible<ProfileId> {
-        let next_idx = self.indexes.iter().cloned().max().unwrap_or(-1) + 1;
         let xsk = self.mercury_xsk()?;
-        let profile_id = Self::profile_id(&xsk, next_idx)?;
-        self.indexes.push(next_idx);
+        let profile_id = Self::profile_id(&xsk, self.next_idx)?;
+        self.active_idx = Option::Some(self.next_idx);
+        self.next_idx += 1;
         debug!("Setting active profile to {}", profile_id);
-        self.active_idx = Option::Some(next_idx);
         Ok(profile_id)
+    }
+
+    fn restore_id(&mut self, id: &ProfileId) -> Fallible<()> {
+        if self.index_of(id).is_some() {
+            return Ok(());
+        }
+
+        for _i in 0..GAP {
+            if *id == self.create_id()? {
+                return Ok(());
+            }
+        }
+
+        bail!("{} is not owned by this seed", id);
     }
 
     fn get_active(&self) -> Fallible<Option<ProfileId>> {
@@ -92,12 +122,8 @@ impl ProfileVault for HdProfileVault {
     }
 
     fn set_active(&mut self, id: &ProfileId) -> Fallible<()> {
-        if let Some(pos) = self
-            .list()?
-            .iter()
-            .position(|candidate_id| candidate_id == id)
-        {
-            self.active_idx = Option::Some(self.indexes[pos]);
+        if let Some(idx) = self.index_of(id) {
+            self.active_idx = Option::Some(idx as i32);
             Ok(())
         } else {
             bail!("Profile Id '{}' not found", id)
