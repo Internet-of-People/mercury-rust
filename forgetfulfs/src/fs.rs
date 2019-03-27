d9b7330808973603c500348bf3c5fc573fe9ca84
wigy <wigy_opensource_developer@yahoo.com>
2019-03-27 16:21:44 +0100

    Add: Read and write file contents

--- forgetfulfs/src/fs.rs
+++ forgetfulfs/src/fs.rs
@@ -26,12 +26,14 @@ struct FsImpl {
 impl FsImpl {
     fn new(uid: u32, gid: u32) -> Self {
         let mut entries = HashMap::with_capacity(128);
-        entries.insert(
-            PathBuf::from("/"),
-            Entry::Dir {
-                mtime_sec: Self::now_utc(),
-            },
-        );
+        let mtime_sec = Self::now_utc();
+        entries.insert(PathBuf::from("/"), Entry::Dir { mtime_sec });
+        entries.insert(PathBuf::from("/hello.txt"), Entry::File { mtime_sec, blob: b"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel sem in mi tempus facilisis. Sed dapibus est quis ante placerat, sit amet luctus erat ornare. Duis finibus orci risus, id volutpat dui consectetur nec. Nullam ultrices non sapien in hendrerit. Aenean sit amet massa risus. Donec sit amet lorem mauris. Donec sit amet tristique eros. Phasellus sit amet nulla elit. Cras nibh tellus, imperdiet vitae molestie quis, placerat pellentesque lorem. Vestibulum hendrerit ipsum ut est malesuada mattis. Donec elementum efficitur quam et tincidunt. Mauris malesuada nulla neque, id tempor ligula placerat in. Phasellus nulla elit, tristique ornare hendrerit et, sodales a purus. Cras hendrerit dui ac elit porta, id commodo mauris faucibus. Nullam porttitor suscipit varius.
+Donec dictum, massa ac consequat imperdiet, ligula tellus varius justo, et pulvinar velit quam vitae mi. Maecenas ac lorem nunc. Aenean lacinia dignissim tortor placerat tincidunt. Ut non dui ut dolor auctor hendrerit. Aenean vitae dui et nisi aliquam molestie non in lorem. Morbi sollicitudin enim sit amet consequat dictum. Phasellus lobortis rutrum mi, ac ornare metus mattis sit amet. Donec nec fringilla odio. Donec vehicula, sem sit amet tincidunt porttitor, neque tellus blandit nibh, in sollicitudin mauris orci quis lacus. Donec elit felis, congue vel facilisis quis, sodales nec ipsum. Curabitur hendrerit leo lectus, pretium efficitur eros vulputate eget. Aenean bibendum dui ut ex pretium, eget feugiat lacus auctor. Curabitur ut erat lectus. Cras vitae augue blandit, tincidunt ex in, feugiat ipsum. Duis lacinia risus nunc, eget consequat massa vulputate elementum. Mauris at pellentesque leo, in mattis nisi.
+Morbi eros nibh, tempus sit amet enim eu, finibus suscipit enim. Sed quis libero diam. Duis accumsan et purus at posuere. Nulla aliquam tincidunt ante, egestas bibendum nunc blandit at. Aliquam sit amet eleifend leo, vel gravida tellus. Donec lacus justo, rutrum vitae mattis vitae, congue nec ipsum. Sed varius lacinia est, sed scelerisque diam commodo in. Nullam consequat, purus eu dignissim suscipit, ex quam lacinia urna, posuere euismod turpis quam eget nulla.
+Nullam blandit neque erat, eget placerat enim porttitor non. Nullam vel est at nunc tincidunt iaculis non eget ipsum. Donec tristique tempus consectetur. Quisque iaculis lectus ut odio vulputate, quis condimentum sapien pharetra. Phasellus ultricies lorem at neque ultrices, id varius diam imperdiet. Duis ultrices tellus non felis tempus molestie. Ut consectetur id arcu at gravida. Etiam mauris ipsum, ultrices et mollis at, mollis eget lectus. Donec posuere velit quis nibh rutrum tempus. Quisque erat nunc, pretium efficitur vulputate at, pharetra eu arcu. Sed erat lacus, iaculis eget purus a, vehicula bibendum mauris. Sed ultricies varius ligula, at tempor lacus vulputate lobortis. Suspendisse potenti.
+Vestibulum sagittis est dolor, nec euismod massa rhoncus sed. Mauris vel arcu lobortis, aliquam leo a, elementum urna. Curabitur et nisl quis velit cursus condimentum ac id neque. Praesent rhoncus mi eget tellus malesuada maximus. Maecenas nec porta eros, id placerat mi. Cras a urna gravida, imperdiet eros in, lobortis tellus. Morbi viverra, odio eu malesuada iaculis, tellus felis posuere mi, quis aliquam dolor eros eget enim. Mauris lacinia volutpat ipsum. Fusce eget auctor mauris. In dolor ex, pharetra eu purus ac, auctor vestibulum purus. Donec ultricies mollis diam, et rhoncus est euismod a. Pellentesque ut metus non nulla luctus condimentum. Etiam quis lectus porta orci sagittis imperdiet.
+".to_vec() });
         FsImpl { uid, gid, entries }
     }
 
@@ -55,6 +57,13 @@ impl FsImpl {
         }
     }
 
+    fn blob(&self, path: &Path) -> Result<&Blob, LibCError> {
+        match self.entries.get(path).ok_or(libc::ENOENT)? {
+            Entry::Dir { .. } => Err(libc::EISDIR),
+            Entry::File { blob, .. } => Ok(blob),
+        }
+    }
+
     fn truncate(&mut self, path: &Path, size: u64) -> ResultEmpty {
         let blob = self.blob_mut(path)?;
         blob.resize_with(size as usize, Default::default);
@@ -62,15 +71,33 @@ impl FsImpl {
     }
 
     fn utimens(&mut self, path: &Path, mtime: Option<Timespec>) -> ResultEmpty {
-        let entry: &mut Entry = self.entries.get_mut(path).ok_or(libc::ENOENT)?;
-        let x: &mut i64 = match entry {
-            Entry::Dir { mtime_sec } => mtime_sec,
-            Entry::File { mtime_sec, .. } => mtime_sec,
+        let entry = self.entries.get_mut(path).ok_or(libc::ENOENT)?;
+        let x = match entry {
+            Entry::Dir { mtime_sec } | Entry::File { mtime_sec, .. } => mtime_sec,
         };
         if mtime.is_some() {
             *x = mtime.unwrap().sec;
         }
         Ok(())
+    }
+
+    fn read(&self, path: &Path, offset: u64, size: u32) -> ResultData {
+        let blob = self.blob(path)?;
+        Ok(blob
+            .iter()
+            .skip(offset as usize)
+            .take(size as usize)
+            .cloned()
+            .collect())
+    }
+
+    fn write(&mut self, path: &Path, offset: u64, data: Vec<u8>) -> ResultWrite {
+        let blob = self.blob_mut(path)?;
+        let count = data.len();
+        let start = usize::min(offset as usize, blob.len());
+        let end = usize::min(offset as usize + data.len(), blob.len());
+        let _discarded: Vec<u8> = blob.splice(start..end, data).collect();
+        Ok(count as u32)
     }
 
     fn unlink(&mut self, parent: &Path, name: &OsStr) -> ResultEmpty {
@@ -311,6 +338,36 @@ impl FilesystemMT for ForgetfulFS {
             size
         );
         self.wlock(req, |this| this.truncate(path, size))
+    }
+
+    fn read(&self, req: RequestInfo, path: &Path, _fh: u64, offset: u64, size: u32) -> ResultData {
+        info!(
+            "{}: read {}, {}, {}",
+            req.unique,
+            path.to_string_lossy(),
+            offset,
+            size
+        );
+        self.rlock(req, |this| this.read(path, offset, size))
+    }
+
+    fn write(
+        &self,
+        req: RequestInfo,
+        path: &Path,
+        _fh: u64,
+        offset: u64,
+        data: Vec<u8>,
+        _flags: u32,
+    ) -> ResultWrite {
+        info!(
+            "{}: read {}, {}, {}",
+            req.unique,
+            path.to_string_lossy(),
+            offset,
+            data.len()
+        );
+        self.wlock(req, |this| this.write(path, offset, data))
     }
 
     fn utimens(
