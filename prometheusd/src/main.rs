40e053f6da76a2bbd15f7fe9ab7c0119a4a340b0
Rache Bartmoss <bartmoss@tutanota.com>
2019-06-13 16:15:03 +0200

    Built sample service that validates BIP39 words

--- prometheusd/src/main.rs
+++ prometheusd/src/main.rs
@@ -1,11 +1,22 @@
-use actix_web::{web, App, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
-fn index(info: web::Path<(u32, String)>) -> impl Responder {
-    format!("Hello {}! id:{}", info.1, info.0)
+use keyvault::Seed;
+
+fn validate_bip39_word(path: web::Path<(String,)>) -> impl Responder {
+    let word = &path.0;
+    let is_valid = Seed::check_word(word);
+    let content = format!("Validity of word '{}': {}", word, is_valid);
+    if is_valid {
+        HttpResponse::Accepted().body(content)
+    } else {
+        HttpResponse::NotAcceptable().body(content)
+    }
 }
 
 fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(web::resource("/{id}/{name}/index.html").to(index)))
-        .bind("127.0.0.1:8080")?
-        .run()
+    HttpServer::new(|| {
+        App::new().service(web::resource("/validate_bip39_word/{word}").to(validate_bip39_word))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
 }
