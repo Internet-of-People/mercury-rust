0c87b243582d78a87fa90394ceb049ae35e89b42
Rache Bartmoss <bartmoss@tutanota.com>
2019-06-14 09:36:18 +0200

    Added endpoints to generate and validate whole phrases

--- prometheusd/src/main.rs
+++ prometheusd/src/main.rs
@@ -2,20 +2,31 @@ use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 use keyvault::Seed;
 
-fn validate_bip39_word(path: web::Path<(String,)>) -> impl Responder {
-    let word = &path.0;
-    let is_valid = Seed::check_word(word);
-    let content = format!("Validity of word '{}': {}", word, is_valid);
-    if is_valid {
-        HttpResponse::Accepted().body(content)
-    } else {
-        HttpResponse::NotAcceptable().body(content)
+fn generate_bip39_phrase() -> impl Responder {
+    Seed::generate_bip39()
+}
+
+fn validate_bip39_phrase(phrase: web::Path<String>) -> impl Responder {
+    match Seed::from_bip39(&phrase as &str) {
+        Ok(_seed) => HttpResponse::Accepted().body("Valid"),
+        Err(e) => HttpResponse::NotAcceptable().body(format!("Invalid: {}", e)),
     }
+}
+
+fn validate_bip39_word(word: web::Path<String>) -> impl Responder {
+    let is_valid = Seed::check_word(&word);
+    let mut resp = if is_valid { HttpResponse::Accepted() } else { HttpResponse::NotAcceptable() };
+    resp.body(is_valid.to_string())
 }
 
 fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(web::resource("/validate_bip39_word/{word}").to(validate_bip39_word))
+        App::new().service(
+            web::scope("/vault")
+                .service(web::resource("/generate_phrase").to(generate_bip39_phrase))
+                .service(web::resource("/validate_phrase/{phrase}").to(validate_bip39_phrase))
+                .service(web::resource("/validate_word/{word}").to(validate_bip39_word)),
+        )
     })
     .bind("127.0.0.1:8080")?
     .run()
