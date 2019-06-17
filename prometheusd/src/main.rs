f5ca7b1a5ee248a5b6607e5380fb3c1860f8b93b
Rache Bartmoss <bartmoss@tutanota.com>
2019-06-17 13:02:14 +0200

    Added runner thread so as daemon can be used from different environments (e.g. JS)

--- prometheusd/src/main.rs
+++ prometheusd/src/main.rs
@@ -19,15 +19,26 @@ fn validate_bip39_word(word: web::Path<String>) -> impl Responder {
     resp.body(is_valid.to_string())
 }
 
-fn main() -> std::io::Result<()> {
+fn run_daemon(listen_on: &str) -> std::io::Result<()> {
+    // NOTE HTTP server already handles signals internally unless the no_signals option is set.
     HttpServer::new(|| {
-        App::new().service(
-            web::scope("/vault")
-                .service(web::resource("/generate_phrase").to(generate_bip39_phrase))
-                .service(web::resource("/validate_phrase/{phrase}").to(validate_bip39_phrase))
-                .service(web::resource("/validate_word/{word}").to(validate_bip39_word)),
-        )
+        App::new()
+            .service(
+                web::scope("/vault")
+                    .service(web::resource("/generate_phrase").to(generate_bip39_phrase))
+                    .service(web::resource("/validate_phrase/{phrase}").to(validate_bip39_phrase))
+                    .service(web::resource("/validate_word/{word}").to(validate_bip39_word)),
+            )
+            .default_service(web::to(|| HttpResponse::NotFound()))
     })
-    .bind("127.0.0.1:8080")?
+    .bind(listen_on)?
     .run()
+}
+
+fn main() {
+    match std::thread::spawn(|| run_daemon("127.0.0.1:8080")).join() {
+        Err(e) => println!("Daemon thread failed with error: {:?}", e),
+        Ok(Err(e)) => println!("Web server failed with error: {:?}", e),
+        Ok(Ok(())) => println!("Gracefully shut down"),
+    };
 }
